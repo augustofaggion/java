50aa370ca71c4f0917cc36c2529ee8bc517f4a6f
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-10-01 13:26:33 +0200

    added new algorithm

--- CheckPermutation.java
+++ CheckPermutation.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+
 public class CheckPermutation {
-  // Split using for
-  // push to an array
-  // sort the array
-  // compare the two arrays
   
+    static boolean checkPermutation(String s1, String s2) {
+      if (s1.length() != s2.length()) {
+        return false;
+      }
+      char[] s1Array = s1.toCharArray();
+      char [] s2Array = s2.toCharArray();
+
+      Arrays.sort(s1Array);
+      Arrays.sort(s2Array);
+
+      return Arrays.equals(s1Array, s2Array);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(checkPermutation("hello", "world")); // false
+    System.out.println(checkPermutation("world", "dlrow")); // true
+  }
 }
